571ad2d626e19b86c874e923507c78fc6f630c18
mario <mnelli@sogei.it>
2021-03-16 15:52:12 +0100

    init

---
+++ ci/ciApp.swift
@@ -0,0 +1,17 @@
+//
+//  ciApp.swift
+//  ci
+//
+//  Created by macbookpro on 16/03/21.
+//
+
+import SwiftUI
+
+@main
+struct ciApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
